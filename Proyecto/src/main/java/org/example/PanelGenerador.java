ad7d3ff8e464d664067d40b7ce2b10ee6ea4f37e
Felipegarciah <fegarcia2024@udec.cl>
2025-06-20 14:05:03 -0400

    bases para panel generador

--- Proyecto/src/main/java/org/example/PanelGenerador.java
+++ Proyecto/src/main/java/org/example/PanelGenerador.java
@@ -1,6 +1,26 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PanelGenerador extends JPanel {
+    /**
+    * presionar el boton generar genera un nuevo jframe que muestre la distribucion del torneo
+    * lo que deberia cerrar este jframe pero primero hace la pregunta de si esta seguro de
+     * continuar. y tambien aca deberia estar el creador del siguiente frame
+    */
+    //boton
+    private JButton confirmar;
+
+    public PanelGenerador() {
+        setLayout(null);
+        setBackground(Color.darkGray);
+
+        this.confirmar = new JButton("Generar");
+        confirmar.setBackground(Color.green);
+        confirmar.setBounds(125, 100, 250, 200);
+
+
+        add(confirmar);
+    }
 }
