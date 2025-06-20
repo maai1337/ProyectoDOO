4db3dc6ab718f74ba82850c8f976a8cbdd6f3d84
Felipegarciah <fegarcia2024@udec.cl>
2025-06-20 14:03:19 -0400

    se agregan botones y mejora en la seguridad

--- Proyecto/src/main/java/org/example/PanelTipoFase.java
+++ Proyecto/src/main/java/org/example/PanelTipoFase.java
@@ -3,18 +3,36 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PanelTipoFase extends JPanel {
+
+    // botones de solo una eleccion
+    private JRadioButton opcion1;
+    private JRadioButton opcion2;
+    private JRadioButton opcion3;
+
+
     public PanelTipoFase() {
         setBackground(Color.ORANGE );
         setLayout(null);
 
-        JRadioButton opcion1 = new JRadioButton("Fase de grupos");
+        this.opcion1 = new JRadioButton("Fase de grupos");
         opcion1.setBackground(Color.orange );
         opcion1.setBounds(20,40,800,40);
-        JRadioButton opcion2 = new JRadioButton("Fase de eliminatoria");
+        this.opcion2 = new JRadioButton("Fase de eliminatoria");
         opcion2.setBackground(Color.orange );
         opcion2.setBounds(20,70,800,40);
+        this.opcion3 = new JRadioButton("Fase de puntos");
+        // tabla de clasificacion  ej: los de la formula 1
+        opcion3.setBackground(Color.orange );
+        opcion3.setBounds(20,100,800,40);
+
+        //agrupar botones
+        ButtonGroup opciones = new ButtonGroup();
+        opciones.add(opcion1);
+        opciones.add(opcion2);
+        opciones.add(opcion3);
 
         add(opcion1);
         add(opcion2);
+        add(opcion3);
     }
 }
