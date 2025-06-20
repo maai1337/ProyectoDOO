b870f9f53bd41313188394b9f8340fafd2cf9164
Felipegarciah <fegarcia2024@udec.cl>
2025-06-20 14:02:29 -0400

    se mejoro la seguridad del codigo

--- Proyecto/src/main/java/org/example/PanelAsistentes.java
+++ Proyecto/src/main/java/org/example/PanelAsistentes.java
@@ -4,35 +4,48 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PanelAsistentes extends JPanel {
-    public PanelAsistentes() {
+
+   //recordar luego cambiar a frabric
+   //spinner para ingresar numero de parti
+   private JSpinner cantidad_participantes;
+
+   //texto informativo
+   private JLabel cantidad;
+   private JLabel restriccion;
+
+   // boton que ocnfirme para pasar el estado a siguiente pregunta
+   private JButton aceptar;
+
+
+   public PanelAsistentes() {
         setBackground(Color.gray);
         setLayout(null);
 
-       JLabel lblAsistente = new JLabel("Cantidad de participantes:");
-       lblAsistente.setFont(new Font("Serif", Font.PLAIN, 26));
-       lblAsistente.setBounds(100,80,2000,50);
-       lblAsistente.setForeground(Color.white);
+       this.cantidad=new JLabel("Cantidad");
+       cantidad.setFont(new Font("Serif", Font.PLAIN, 26));
+       cantidad.setBounds(100,80,2000,50);
+       cantidad.setForeground(Color.white);
        SpinnerNumberModel model = new SpinnerNumberModel(4,2,50,1);
-       JSpinner spnAsistente = new JSpinner(model);
-       spnAsistente.setBounds(150,140,100,30);
+       this.cantidad_participantes = new JSpinner(model);
+       cantidad_participantes.setBounds(150,140,100,30);
         // en teoria esto es apra accceder al editor del texto del spin
-       JComponent jcomp = spnAsistente.getEditor();
+       JComponent jcomp = cantidad_participantes.getEditor();
        JFormattedTextField ftfAsistente = ((JSpinner.DefaultEditor) jcomp).getTextField();
        jcomp.setBackground(Color.white);
        ftfAsistente.setFont(new Font("Serif", Font.PLAIN, 20)) ;
+       this.restriccion  = new JLabel("Minimo 2 y maximo 50 participantes");
 
-       JLabel lblAsistente2 = new JLabel("Minimo 2 y maximo 50 participantes");
-       lblAsistente2.setBounds(100,180,2000,50);
+      restriccion.setBounds(100,180,2000,50);
 
-       JButton confirmar = new JButton("Confirmar");
-       confirmar.setBackground(Color.white);
-       confirmar.setBounds(350,250,100,30) ;
+       this.aceptar = new JButton("Confirmar");
+      aceptar.setBackground(Color.white);
+      aceptar.setBounds(350,250,100,30) ;
 
 
-       add(confirmar);
-       add(lblAsistente2);
-       add(lblAsistente);
-       add(spnAsistente);
+       add(aceptar);
+       add(restriccion);
+       add(cantidad);
+       add(cantidad_participantes);
     }
 
 }
