68cb9fd97c7706af8e5a0593809e454e107e504d
Felipegarciah <fegarcia2024@udec.cl>
2025-06-21 21:08:17 -0400

    se creo forma de cambio de estado y habilitar paneles

--- Proyecto/src/main/java/org/example/PantallaInicial.java
+++ Proyecto/src/main/java/org/example/PantallaInicial.java
@@ -1,10 +1,10 @@
 package org.example;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class PantallaInicial extends JPanel {
+
+    //paneles donde se configura
     private PanelAsistentes panelAistentes; //cambiar nombres
     private PanelTipoFase panelTipoFase;
     private PanelDatos panelDatos;
@@ -12,16 +12,31 @@ public class PantallaInicial extends JPanel {
     private PanelNombre panelNombre;
     private PanelGenerador panelGenerador;
 
+    //estados que habilitaran los paneles
+    private enum EstadoPantalla {
+        NOMBRE,
+        ASISTENTES,
+        DATOS,
+        FASE,
+        DISTRIBUCION,
+        GENERADOR,
+        COMPLETADO
+    }
 
-    public PantallaInicial(){
-        GridLayout Inicio = new GridLayout(2,3);
+    private EstadoPantalla estadoActual;
+
+
+    public PantallaInicial() {
+        GridLayout Inicio = new GridLayout(2, 3);
         setLayout(Inicio);
-        panelAistentes=new PanelAsistentes();
-        panelDatos=new PanelDatos();
-        panelTipoFase=new PanelTipoFase();
-        panelDistribucion =new PanelDistribucion();
-        panelNombre = new PanelNombre();
+
+        panelAistentes = new PanelAsistentes();
+        panelDatos = new PanelDatos();
+        panelTipoFase = new PanelTipoFase();
+        panelDistribucion = new PanelDistribucion();
+        panelNombre = new PanelNombre(this);
         panelGenerador = new PanelGenerador();
+
         add(panelNombre);
         add(panelAistentes);
         add(panelDatos);
@@ -29,8 +44,63 @@ public class PantallaInicial extends JPanel {
         add(panelDistribucion);
         add(panelGenerador);
 
+        cambiarEstadoActual(1);
     }
 
+    public void cambiarEstadoActual(int a) {
+
+        setPanelEnabled(panelNombre,false);
+        setPanelEnabled(panelAistentes,false);
+        setPanelEnabled(panelDatos,false);
+        setPanelEnabled(panelTipoFase,false);
+        setPanelEnabled(panelDistribucion,false);
+        setPanelEnabled(panelGenerador,false);
+
+        //activamos el estado en que nos encontremos
+        switch (a) {
+            case 1:
+                setPanelEnabled(panelNombre,true);
+
+                break;
+            case 4:
+                setPanelEnabled(panelAistentes,true);
+
+                break;
+            case 3:
+                setPanelEnabled(panelDatos,true);
+
+                break;
+            case 2:
+                setPanelEnabled(panelTipoFase,true);
+
+                break;
+            case 5:
+                setPanelEnabled(panelDistribucion,true);
+
+                break;
+            case 6:
+                setPanelEnabled(panelGenerador,true);
+
+                break;
+
+            case 7:
+                JOptionPane.showMessageDialog(null, "ver luego como crear el segundo frame");
+                break;
+        }
+        repaint();
+        revalidate();
+
+
+
+    }
+    //esto desactiva el panel y lo que contenga
+    public void setPanelEnabled(JPanel panel, boolean interruptor) {
+        panel.setEnabled(interruptor);
+        for (Component comp : panel.getComponents()) {
+            comp.setEnabled(interruptor);
+
+        }
+    }
 
 
 
